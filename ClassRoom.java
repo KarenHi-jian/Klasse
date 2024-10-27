11a6511f8b9ac9b2dd93709a7df744901abf3a35
karen Hi-Jian Garcia Pinal <gar240730@spengergasse.at>
2024-10-27 17:37:10 +0100

    AKTUELL

--- ClassRoom.java
+++ ClassRoom.java
@@ -1,5 +1,4 @@
-
-public class ClassRoom
+ public class ClassRoom
 {
     private String  roomNr;     // consists of building floor and room number
     private boolean computer;   // are there computers in the room
@@ -10,27 +9,58 @@ public class ClassRoom
      * Parameters: Room number, computer, size
      * It is creating a new Insatance of the class ClassRoom
      */
-    public ClassRoom(String newRoomNr, boolean newComputer, int newSize)
+    public ClassRoom(String roomNr, boolean computer, int size)
     {
-        setRoomNr(newRoomNr);
-        setComputer(newComputer);
-        setSize(newSize);
+        setRoomNr(roomNr);
+        setComputer(computer);
+        setSize(size);
     }
     
-    public void setRoomNr(String newRoomNr)
+    public ClassRoom(String roomNr, boolean computer)
+    {
+        setRoomNr(roomNr);
+        setComputer(computer);
+        setSize(25);
+    }
+    
+    public ClassRoom(String roomNr)
+    {
+        setRoomNr(roomNr);
+        setComputer(false);
+        setSize(25);
+    }
+
+    public ClassRoom()
+    {
+        setRoomNr("A.4.11");
+        setComputer(false);
+        setSize(25);
+        
+    }
+  
+    public void setRoomNr(String roomNr)
     {
         // set the property roomNr to the value of the parameter newRoomNr
-        roomNr = newRoomNr;
+        this.roomNr = roomNr;
     }
     
-    public void setComputer(boolean newComputer)
+    public void setComputer(boolean computer)
     {
-        computer = newComputer;
+        this.computer = computer;
     }
     
-    public void setSize(int newSize)
+    public void setSize(int size)
     {
-        size = newSize;
+        // 10 .. 40
+        if ((size >= 10) && (size <= 40))
+        {
+            this.size = size;
+        }
+        else
+        {
+            System.out.println("Error: wrong size. Should be between 10 and 40!");
+            this.size = 25;
+        }
     }
     
     public String getRoomNr()
@@ -46,13 +76,54 @@ public class ClassRoom
     public int getSize()
     {
         return size;
-    }    
-    
-    public void printStudent()
-    { 
-        System.out.println("Classroom " + roomNr + " - Computer: " + computer + " - " + size + " Students");
-        
-        
     }
+
+    public String getFloor()
+    {
+        //0123456789
+        //A.5.20
+        //Extra.2.22
         
+        int pos1;
+        int pos2;
+        String floor;
+        
+        //1
+        //5
+        pos1 = roomNr.indexOf(".") + 1;
+        pos2 = roomNr.indexOf(".", pos1);
+        floor = roomNr.substring(pos1, pos2);
+        
+        return floor;
+    }
+    
+    public void printClassRoom()
+    {
+
+        if (computer == true)
+        {
+            System.out.println("Classroom " + roomNr + " - Computer - " + " - " + size + " Students");
+        }
+        else
+        {
+            System.out.println("Classroom " + roomNr + " - no Computer - " + " - " + size + " Students");
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
